a80b818d45726d0dc6e6f7373520711543732f9f
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-03-13 13:14:15 +0300

    c# giriş alıştırması 1

---
+++ c# giriş alıştırması 1/c# giriş alıştırması 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c__giriş_alıştırması_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selam");
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}

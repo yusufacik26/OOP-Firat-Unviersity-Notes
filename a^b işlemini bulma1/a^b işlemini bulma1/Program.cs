08121e2508ece676e7045c8e2f971b7bacd6586c
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-03-13 13:15:55 +0300

    a^b işlemini bulma

---
+++ a^b işlemini bulma1/a^b işlemini bulma1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a_b_işlemini_bulma1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            int sonuc = 1;
+            
+
+            Console.WriteLine("Tabanı girin :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Üssü girin:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < num2; i++) {
+
+                sonuc *= num1 ;
+            
+            }
+
+            Console.WriteLine("{0}^{1} = {2}", num1, num2, sonuc);
+
+        }
+    }
+}
---
+++ a^b işlemini bulma1/a^b işlemini bulma1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("a^b işlemini bulma1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("a^b işlemini bulma1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("ae9b0026-e421-49b2-b37a-149a92aef749")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

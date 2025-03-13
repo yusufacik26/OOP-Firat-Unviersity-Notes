501fa74e773b82ef80fab81d10949763c66cfe62
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-03-13 13:12:36 +0300

    Klavyeden girilen Stringteki Kelime Sayısını Bulma

---
+++ Klavyeden girilen Stringteki kelime sayısını bulma/Klavyeden girilen Stringteki kelime sayısını bulma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klavyeden_girilen_Stringteki_kelime_sayısını_bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String cumle;
+            Console.WriteLine("Bir cümle girin");
+
+            cumle = Console.ReadLine();
+
+            string[] kelimeler = cumle.Split(' ');
+
+            Console.WriteLine("Cümledeki kelime sayısı " + kelimeler.Length);
+
+
+        }
+    }
+}

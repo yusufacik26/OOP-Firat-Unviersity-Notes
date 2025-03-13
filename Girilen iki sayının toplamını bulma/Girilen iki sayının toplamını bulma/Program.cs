736de9b84d849434555f8b32588b9ae5e094f7f8
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-03-13 13:14:53 +0300

    Girilen iki sayının toplamını bulma

---
+++ Girilen iki sayının toplamını bulma/Girilen iki sayının toplamını bulma/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Girilen_iki_sayının_toplamını_bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2 ,sum;
+
+            Console.WriteLine("1.Sayıyı girin:");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("2.sayıyı girin: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            sum = num1 + num2;
+
+            Console.WriteLine("{0} + {1} ={2}", num1, num2, sum);
+        
+        }
+    }
+}

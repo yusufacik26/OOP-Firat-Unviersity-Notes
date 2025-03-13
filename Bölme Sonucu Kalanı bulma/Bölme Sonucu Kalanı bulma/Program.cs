52807d652ec17df7d19bb6295ec99f15deac94a0
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-03-13 13:15:36 +0300

    Bölme Sonucu Kalanı bulma

---
+++ Bölme Sonucu Kalanı bulma/Bölme Sonucu Kalanı bulma/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bölme_Sonucu_Kalanı_bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bolen = 6;
+                
+            int bolunen=70;
+
+            int sonuc = bolunen / bolen;
+
+            int kalan = bolunen % bolen;
+
+            Console.WriteLine("bolum =" + sonuc);
+            Console.WriteLine("Kalan = " + kalan);
+
+
+        }
+    }
+}

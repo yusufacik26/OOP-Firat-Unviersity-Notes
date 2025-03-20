1ea1edddfdf38e8da72dc5dbd894e2469db66d27
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-03-20 22:40:51 +0300

    Tranlate uyg1

---
+++ Translate/Translate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Sayilar
+{
+    string[] sayilar = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+
+    public string this[int index]
+    {
+        get
+        {
+            if (index >= 0 && index < sayilar.Length)
+                return sayilar[index];
+            else
+                return "Invalid Number";
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Sayilar sayi = new Sayilar();
+        Console.Write("Bir rakam gir (0-9): ");
+        int girdi = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("İngilizce: " + sayi[girdi]);
+    }
+}

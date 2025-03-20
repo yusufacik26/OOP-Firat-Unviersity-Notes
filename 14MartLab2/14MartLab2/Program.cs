c54187656d4a071cd3e7a5b17cff3bd846bf5c6b
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-03-20 15:13:50 +0300

    Dosyaya yazı yazdırma işlemi

---
+++ 14MartLab2/14MartLab2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _14MartLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Int64 x;
+            String line;
+            try {// yazma kısmı 
+                StreamWriter sw = new StreamWriter("C:\\Users\\yusuf\\Desktop\\Yeni klasör\\Text.txt");
+                
+                for (x = 0; x < 10; x++) {
+
+                    sw.Write(x+"\n");
+                
+                }
+                sw.Close();
+
+                StreamReader sr = new StreamReader("C:\\Users\\yusuf\\Desktop\\Yeni klasör\\Text.txt");
+                line = sr.ReadLine();
+
+                while(line != null) {
+
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();// her seferinde yeni satıra geçmesini sağlar
+                }
+                sr.Close();
+            }
+            catch(Exception e) { }
+        }
+    }
+}

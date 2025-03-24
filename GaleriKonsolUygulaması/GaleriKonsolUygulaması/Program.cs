fdbc9d0708db37a711967f8f1d1b18910a458907
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-03-24 13:04:24 +0300

    galeri konsol uyg

---
+++ GaleriKonsolUygulaması/GaleriKonsolUygulaması/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaleriKonsolUygulaması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Araba
+    {
+        private string model;
+        private string marka;
+        private string renk;
+
+        public Araba(string model, string marka,string renk)
+        {
+            this.model = model;
+            this.marka = marka;
+            this.renk = renk;
+
+
+        }
+        public override string ToString()
+        {
+            return marka+" - "+model+" - "renk;
+        }
+
+
+
+    }
+}
---
+++ GaleriKonsolUygulaması/GaleriKonsolUygulaması/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("GaleriKonsolUygulaması")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GaleriKonsolUygulaması")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("f38615e6-fe7f-4e30-a254-ba5da69d1e43")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

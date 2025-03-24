59718ece53ff4b03e7ba5f7d72572232a6916cc8
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-03-24 13:03:44 +0300

    Galeri oop çalışması form uyg

---
+++ Galeri_Form_Uygulaması/Galeri_Form_Uygulaması/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Galeri_Form_Uygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Ekle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(258, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(428, 63);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Marka";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(285, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Model";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(473, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Renk";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(175, 238);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Ekle
+            // 
+            this.Ekle.Location = new System.Drawing.Point(220, 133);
+            this.Ekle.Name = "Ekle";
+            this.Ekle.Size = new System.Drawing.Size(174, 41);
+            this.Ekle.TabIndex = 7;
+            this.Ekle.Text = "ekle";
+            this.Ekle.UseVisualStyleBackColor = true;
+            this.Ekle.Click += new System.EventHandler(this.Ekle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Ekle);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Ekle;
+    }
+}
+
---
+++ Galeri_Form_Uygulaması/Galeri_Form_Uygulaması/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Galeri_Form_Uygulaması
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Ekle_Click(object sender, EventArgs e)
+        {
+            string marka = textBox1.Text;
+            string model = textBox2.Text;
+            string renk = textBox3.Text;
+
+            Araba yeniAraba = new Araba(marka, model, renk);
+
+            Galeri galeri1 = new Galeri();
+
+            galeri1.ArabaEkle(yeniAraba);
+
+            listBox1.Items.Add(yeniAraba.ToString());
+        }
+    }
+    class Araba
+    {
+        private string marka;
+        private string model;
+        private string renk;
+
+
+        public Araba(string marka, string model, string renk)
+        {
+            this.marka = marka;
+            this.model = model;
+            this.renk = renk;
+
+
+        }
+
+        public override string ToString()
+        {
+            return marka + "- " + model + " -" + renk;
+        }
+
+
+    }
+
+    class Galeri
+    {
+
+        private List<Araba> arabalar = new List<Araba>(); // Galeriye arabalar sınıfından kolay araba ekleme
+
+        public void ArabaEkle(Araba a)
+        {
+            arabalar.Add(a);
+
+        }
+
+        public List<Araba> Listele()
+        {
+
+            return arabalar;
+        }
+    }
+}

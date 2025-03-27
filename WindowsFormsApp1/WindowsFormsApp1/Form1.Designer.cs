7d2e3195fdcc03d0a931abb5289f2f4010bb9fe8
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-03-27 12:56:15 +0300

    dosyayı okusun gridview ekle gridview üzerinden ekleme çıkartma yap form kapanınca  gridviewi dosyaya yazdııran uygulama

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Numara = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Adı = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Soyadı = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(537, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.Text = "ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(183, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(325, 81);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Numara,
+            this.Adı,
+            this.Soyadı});
+            this.dataGridView1.Location = new System.Drawing.Point(71, 226);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(354, 150);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Numara
+            // 
+            this.Numara.HeaderText = "Numara";
+            this.Numara.Name = "Numara";
+            // 
+            // Adı
+            // 
+            this.Adı.HeaderText = "Adı";
+            this.Adı.Name = "Adı";
+            // 
+            // Soyadı
+            // 
+            this.Soyadı.HeaderText = "Soyadı";
+            this.Soyadı.Name = "Soyadı";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(537, 146);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Numara;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Adı;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Soyadı;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        private string filePath = "C:\\Users\\yusuf\\Desktop\\Yeni klasör\\file.txt"; // Dosya yolu
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        { 
+            
+            dataGridView1.ColumnCount = 3;
+
+            dataGridView1.Columns[0].Name = "Numara";
+            dataGridView1.Columns[1].Name = "Adı";
+            dataGridView1.Columns[2].Name = "Soyadı";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToDeleteRows = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].Name = "Numara";
+            dataGridView1.Columns[1].Name = "Adı";
+            dataGridView1.Columns[2].Name = "Soyadı";
+
+            // Dosyadan verileri oku ve DataGridView'e yükle
+            if (File.Exists(filePath))
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] data = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (data.Length == 3)
+                        {
+                            dataGridView1.Rows.Add(data[0], data[1], data[2]);
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            string numara = textBox1.Text.Trim();
+            string ad = textBox2.Text.Trim();
+            string soyad = textBox3.Text.Trim();
+
+            if (!string.IsNullOrEmpty(numara) && !string.IsNullOrEmpty(ad) && !string.IsNullOrEmpty(soyad))
+            {
+                // Yeni veriyi DataGridView'e ekle
+                dataGridView1.Rows.Add(numara, ad, soyad);
+
+                // TextBox'ları temizle
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurun!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                if (!row.IsNewRow)
+                {
+                    dataGridView1.Rows.Remove(row);
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string filePath = "C:\\Users\\yusuf\\Desktop\\Yeni klasör\\file.txt";
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        if (!row.IsNewRow) // Yeni satırları kaydetmez
+                        {
+                            string numara = row.Cells[0].Value?.ToString() ?? "";
+                            string ad = row.Cells[1].Value?.ToString() ?? "";
+                            string soyad = row.Cells[2].Value?.ToString() ?? "";
+                            writer.WriteLine($"{numara} {ad} {soyad}");
+                        }
+                    }
+                }
+                MessageBox.Show("Veriler başarıyla dosyaya yazıldı!", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].Name = "Numara";
+            dataGridView1.Columns[1].Name = "Adı";
+            dataGridView1.Columns[2].Name = "Soyadı";
+
+            // FormClosing olayını ekliyoruz
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+        }
+    }
+}
+

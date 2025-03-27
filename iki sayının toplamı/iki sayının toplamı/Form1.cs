3e0d0f5cbaa38922d449c81e94f7164d1c99ae10
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-03-27 04:28:44 +0300

    iki sayının toplamı konsol uygulama

---
+++ iki sayının toplamı/iki sayının toplamı/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iki_sayının_toplamı
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+
+            textBox3.Text = Convert.ToString(sayi1 + sayi2);
+        }
+    }
+}

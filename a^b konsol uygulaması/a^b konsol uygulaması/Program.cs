ec0d9e3e520eb89d0f75316da2a900518b883186
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-03-27 04:27:32 +0300

    a^b

---
+++ a^b konsol uygulaması/a^b konsol uygulaması/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a_b_konsol_uygulaması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("taban sayısını girin...");
+
+            double taban = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Üs sayısını giriniz");
+
+            int us = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0}^{1}={2}", taban, us, Math.Pow(taban, us));
+
+
+        }
+    }
+}

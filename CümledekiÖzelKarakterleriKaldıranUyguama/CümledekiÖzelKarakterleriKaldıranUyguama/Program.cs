a0cfa34a267458097d7e3efc0d19594dacf4b38d
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-03 16:26:06 +0300

    CümledekiÖzelKarakterleriKaldıranUyguama

---
+++ CümledekiÖzelKarakterleriKaldıranUyguama/CümledekiÖzelKarakterleriKaldıranUyguama/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CümledekiÖzelKarakterleriKaldıranUyguama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] karakter = { '*', '-', '$', '#', ')', '(', '/', '%', '+', '!' };
+
+
+            Console.WriteLine("Bir cümle girinz");
+
+            string cumle = Console.ReadLine();
+            string yeniCumle = "";
+           
+
+            foreach(char k in cumle)
+            {
+
+                if (!(karakter.Contains(k))) {
+
+                    yeniCumle += k;
+                }
+
+                
+
+
+            }
+            Console.WriteLine("Cümlenin temizlenmiş hali"+yeniCumle);
+
+
+
+        }
+    }
+}

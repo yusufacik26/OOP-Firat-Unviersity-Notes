7f67813486920970621f6f6b798910902f6fb970
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-03 16:25:23 +0300

    HarfNotundan 4lük sisteme çevirme

---
+++ HarfNotuİndexleyici/HarfNotuİndexleyici/Program.cs
@@ -0,0 +1,36 @@
+﻿using HarfNotuİndexleyici;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarfNotuİndexleyici
+{
+    class Notlar
+    {
+
+        Dictionary<string, double> not = new Dictionary<string, double>{
+        { "AA", 4.0 },
+        { "BA", 3.5 },
+        { "BB", 3.0 },
+        { "CB", 2.5 },
+        { "CC", 2.0 },
+        { "DC", 1.5 },
+        { "DD", 1.0 },
+        { "FF", 0.0 }
+    };
+
+        public double this[string harf] {
+         get => not.ContainsKey(harf) ? not[harf] : -1; }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        Notlar h = new Notlar();
+      Console.WriteLine(h["BA"]);  // Çıktı: 3.5
+    }
+}

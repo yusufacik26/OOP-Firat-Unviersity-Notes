99f68e23eb153096de67a58f432a773a5dd3e3f5
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-03 16:24:09 +0300

    24 vizesi 3.soru bu

---
+++ vize24.2/vize24.2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vize24._2
+{
+    class Aylar
+    {
+
+        string[] ay = { "", "oca", "sub", "mar", "nis", "may", "haz", "tem", "agu", "eyl", "ekm", "kas", "arl" };
+
+
+        public int ayBul(string girdi) {
+        
+            for(int i = 0; i < ay.Length; i++)
+            {
+
+                if(girdi == ay[i]) {
+                    return i;
+                }
+
+            }
+            return -1;
+        }
+
+        public int this[string girdi] //indexkleme yapıyor adamım
+        {
+            get { return ayBul(girdi); }
+
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aylar ay = new Aylar();
+            while (true)
+            {
+                Console.Write("Herhangi bir ayın ilk üç harfini gir:"); string girdi = Console.ReadLine(); if (girdi == "cik") break;
+                int aysirasi = ay[girdi];
+                if (aysirasi > 0) Console.WriteLine("Girilen ay sırası:" + aysirasi); else Console.WriteLine("Geçersiz Giriş");
+            }
+            Console.WriteLine("Devam etmek için bir tuşa basınız"); Console.ReadKey();
+        }
+    }
+}
---
+++ vize24.2/vize24.2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("vize24.2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("vize24.2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("c35e3c7e-bbb6-45a4-ba59-cc66074da125")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

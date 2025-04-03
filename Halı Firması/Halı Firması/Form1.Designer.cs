7de374253148912d5913b3653bce58f0bb4a03c3
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-03 16:22:46 +0300

    Halı firması basic uygulama

---
+++ Halı Firması/Halı Firması/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Halı_Firması
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(151, 189);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(316, 189);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(491, 189);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(641, 189);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(160, 330);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(446, 190);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Ad";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Fabrika Adı";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Halı Sayısı";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "M2";
+            this.Column4.Name = "Column4";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(184, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Kişi Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(343, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Fabrika Adı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(519, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Halı Sayısı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(681, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "M2";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(309, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(497, 76);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "HALI DÜNYASI";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(792, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 52);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(792, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 52);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Çıkart";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(792, 278);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 83);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Tabloya Ekle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(792, 404);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 83);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Tablodan Çıkart";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(956, 189);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(160, 121);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Dosyayı Tabloya Yaz";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 547);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Halı Firması/Halı Firması/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Halı_Firması
+{
+    public partial class Form1 : Form
+    {
+        private string path = "C:\\Users\\yusuf\\Desktop\\Yeni klasör\\halilar.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad = textBox1.Text;
+
+            string FabrikaAdi = textBox2.Text;
+            int haliAdedi = Convert.ToInt32(textBox3.Text);
+            int m2 = Convert.ToInt32(textBox4.Text);
+
+            hali h = new hali(ad, FabrikaAdi, haliAdedi, m2);
+
+
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine(h.Ad + " " + h.fabrikaadi + " " + h.HaliAdedi + " " + h.M2);
+
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string ad = textBox1.Text;
+            string FabrikaAdi = textBox2.Text;
+            string haliAdedi = Convert.ToString(textBox3.Text);
+            string m2 = Convert.ToString(textBox4.Text);
+
+            List<string> satirlar = new List<string>();
+            bool silindi = false;
+
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string satir;
+                    while ((satir = sr.ReadLine()) != null)
+                    {
+                        // javadaki gibi split işlemi!!!!!
+                        string[] parcalar = satir.Split(' ');
+
+                        if (parcalar.Length == 4 &&
+                            parcalar[0] == ad &&
+                            parcalar[1] == FabrikaAdi &&
+                            parcalar[2] == haliAdedi &&
+                            parcalar[3] == m2)
+                        {
+
+                            silindi = true;
+                            continue;
+                        }
+
+                        // Eşleşmeyen satırları listeye ekledim burda
+                        satirlar.Add(satir);
+                    }
+                }
+
+                // Eğer bir satır silindiyse dosyayı güncelle!!!
+                if (silindi)
+                {
+                    using (StreamWriter sw = new StreamWriter(path, false)) // Dosyanın içeriğini güncelle
+                    {
+                        foreach (string s in satirlar)
+                        {
+                            sw.WriteLine(s);
+                        }
+                    }
+                    MessageBox.Show("Veri başarıyla silindi!");
+                }
+                else
+                {
+                    MessageBox.Show("Belirtilen veri bulunamadı.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Dosya bulunamadı.");
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.Columns[0].Name = "Kişi Adı";
+            dataGridView1.Columns[1].Name = "Fabrika Adı";
+            dataGridView1.Columns[2].Name = "Halı Sayısı";
+            dataGridView1.Columns[3].Name = "M2";
+
+            // Dosyadan verileri oku ve DataGridView'e yükle
+            if (File.Exists(path))
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] data = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (data.Length == 4)
+                        {
+                            dataGridView1.Rows.Add(data[0], data[1], data[2], data[3]);
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            dataGridView1.ColumnCount = 4;
+
+            dataGridView1.Columns[0].Name = "Kişi Adı";
+            dataGridView1.Columns[1].Name = "Fabrika Adı";
+            dataGridView1.Columns[2].Name = "Halı Sayısı";
+            dataGridView1.Columns[3].Name = "M2";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToDeleteRows = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            string ad = textBox1.Text.Trim();
+            string FabrikaAdi = textBox2.Text.Trim();
+            string haliAdedi = Convert.ToString(textBox3.Text);
+            string m2 = Convert.ToString(textBox4.Text);
+
+            if (!string.IsNullOrEmpty(ad) && !string.IsNullOrEmpty(FabrikaAdi) && !string.IsNullOrEmpty(haliAdedi) && !string.IsNullOrEmpty(m2))
+            {
+                // Yeni veriyi DataGridView'e ekle
+                dataGridView1.Rows.Add(ad, FabrikaAdi, haliAdedi, m2);
+
+                // TextBox'ları temizle
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+
+            }
+            else
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurun!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                if (!row.IsNewRow)
+                {
+                    dataGridView1.Rows.Remove(row);
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string filePath = "C:\\Users\\yusuf\\Desktop\\Yeni klasör\\file.txt";
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        if (!row.IsNewRow) // Yeni satırları kaydetmez
+                        {
+                            string ad = row.Cells[0].Value?.ToString() ?? "";
+                            string FabrikaAdi = row.Cells[1].Value?.ToString() ?? "";
+                            string haliAdedi = row.Cells[2].Value?.ToString() ?? "";
+                            string m2 = row.Cells[2].Value?.ToString() ?? "";
+                            writer.WriteLine($"{ad} {FabrikaAdi} {haliAdedi} {m2}");
+                        }
+                    }
+                }
+                MessageBox.Show("Veriler başarıyla dosyaya yazıldı!", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            List<string> satirlar = new List<string>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string satir;
+                while ((satir = sr.ReadLine()) != null)
+                {
+                    // javadaki gibi split işlemi!!!!!
+                    string[] parcalar = satir.Split(' ');
+                    dataGridView1.Rows.Add(parcalar[0], parcalar[1], parcalar[2], parcalar[3]);
+
+
+                }
+            }
+        }
+    }
+
+    class hali
+    {
+        private string ad;
+        private string FabrikaAdi;
+        private int haliAdedi;
+        private int m2;
+
+
+        public hali(string ad, string FabrikaAdi, int haliAdedi, int m2)
+        {
+
+            this.ad = ad;
+            this.FabrikaAdi = FabrikaAdi;
+            this.haliAdedi = haliAdedi;
+            this.m2 = m2;
+
+
+        }
+        public string Ad
+        {
+
+            get { return ad; }
+        }
+
+        public string fabrikaadi
+        {
+            get { return FabrikaAdi; }
+        }
+
+        public int HaliAdedi
+        {
+            get { return haliAdedi; }
+        }
+
+        public int M2
+        {
+            get { return m2; }
+        }
+    }
+}
+    
+
+
+
+
+
+    
+
+

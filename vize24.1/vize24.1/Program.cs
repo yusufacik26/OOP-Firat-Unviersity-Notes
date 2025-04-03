5deea1afb6cd0b6ac47b8bc9aad5748063c859ab
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-03 16:23:23 +0300

    23vizeilksoru

---
+++ vize24.1/vize24.1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vize24._1
+{
+    class kisi
+    {
+
+        public int id;
+        public string isim;
+        public pasaport p;
+
+        public kisi(string isim, int id)
+        {
+            this.isim = isim;
+            this.id = id;
+
+
+        }
+    }
+
+    class pasaport
+    {
+        public string ulkeKodu;
+        public int seriNo;
+        public kisi sahip;
+
+        public pasaport(string ulkeKodu, int seriNo, kisi sahip)
+        {
+            this.ulkeKodu = ulkeKodu;
+            this.seriNo = seriNo;
+            this.sahip = sahip;
+
+        }
+
+    }
+
+    class ucus
+    {
+        List<kisi> kisiler = new List<kisi>();
+
+        public void ekle(kisi binici)
+        {
+            kisiler.Add(binici);
+        }
+
+        public void listele()
+        {
+
+            foreach(kisi k in kisiler)
+            {
+                Console.WriteLine(k.id + " " + k.isim+" "+k.p.ulkeKodu);
+
+            }
+    }
+        class Program
+        {
+
+            static void Main(string[] args)
+            {
+            }
+        }
+    
+}
---
+++ vize24.1/vize24.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("vize24.1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("vize24.1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("fd54c671-56e1-4dc0-9afa-f3da9f72b5a6")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

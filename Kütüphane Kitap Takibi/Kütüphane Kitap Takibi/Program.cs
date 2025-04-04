837908719debd17c55b6467a495839fb8d12e08f
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-04 14:38:29 +0300

    Kütüphane Kitap Takibi

---
+++ Kütüphane Kitap Takibi/Kütüphane Kitap Takibi/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kütüphane_Kitap_Takibi
+{
+
+    class Kitap
+    {
+
+        public string ad;
+        public string yazar;
+        public string ISBN;
+
+        public Kitap(string ad, string yazar, string ıSBN)
+        {
+            this.ad = ad;
+            this.yazar = yazar;
+            this.ISBN = ıSBN;
+        }
+
+        public override string ToString()
+        {
+            return $"Kitap Adı:{ad},Yazar:{yazar},ISBN:{ISBN}";
+           
+        }
+    }
+
+    class Uye
+    {
+        public string adSoyad;
+        List<Kitap> OduncKitaplar = new List<Kitap>();
+
+        public Uye(string adSoyad)
+        {
+            this.adSoyad = adSoyad;
+        }
+        public void Ekle(Kitap k)
+        {
+
+            OduncKitaplar.Add(k);
+
+        }
+
+        public override string ToString()
+        {
+            return $"{adSoyad}";
+            
+        }
+
+        public void Listele()
+        {
+            Console.WriteLine($"\n{adSoyad} adlı üyenin ödünç aldığı kitaplar:");
+            foreach (Kitap k in OduncKitaplar)
+            {
+                Console.WriteLine(k);
+            }
+
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kitaplar
+            Kitap k1 = new Kitap("1984", "George Orwell", "123456");
+            Kitap k2 = new Kitap("Kürk Mantolu Madonna", "Sabahattin Ali", "789101");
+
+            // Üye
+            Uye u1 = new Uye("Zeynep Demir");
+
+            // Kitap ekleme
+            u1.Ekle(k1);
+            u1.Ekle(k2);
+
+            // Listeleme
+            u1.Listele();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Kütüphane Kitap Takibi/Kütüphane Kitap Takibi/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Kütüphane Kitap Takibi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kütüphane Kitap Takibi")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("a6da2795-b794-4fd2-9bb9-501037e32b4a")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

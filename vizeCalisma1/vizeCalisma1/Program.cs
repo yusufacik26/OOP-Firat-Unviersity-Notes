5d73a0514389fb061e71277dd0fa64aea3aeaec9
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-04 14:37:32 +0300

    vizeCalisma1

---
+++ vizeCalisma1/vizeCalisma1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vizeCalisma1
+{
+
+    class Siparis
+    {
+        public string UrunAdi;
+        public int adet;
+        public int fiyat;
+
+
+        public override string ToString() // Bilgileri yazdırmak için önemli!!!!!!
+        {
+            return $"Ürün: {UrunAdi}, Adet: {adet}, Fiyat: {fiyat}₺";
+        }
+
+    }
+
+    class masa
+    {
+
+        List<Siparis> siparisler = new List<Siparis>();
+
+        public void siparisEkle(Siparis s)
+        {
+
+            siparisler.Add(s);
+
+
+        }
+
+        public void Listele() {
+        
+            foreach(Siparis s in siparisler)
+            {
+                Console.WriteLine(s);
+
+
+            }
+        
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            masa masa1 = new masa();
+
+            Siparis s1 = new Siparis { UrunAdi = "Pizza", adet = 2, fiyat = 100 };
+            Siparis s2 = new Siparis { UrunAdi = "Kola", adet = 1, fiyat = 20 };
+            Siparis s3 = new Siparis { UrunAdi = "Kola", adet = 1, fiyat = 20 };
+
+            masa1.siparisEkle(s1);
+            masa1.siparisEkle(s2);
+            masa1.siparisEkle(s3);
+
+            masa1.Listele();
+        }
+    }
+}
---
+++ vizeCalisma1/vizeCalisma1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("vizeCalisma1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("vizeCalisma1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("c378b808-6ae7-4f20-ac7e-46fe2668e64d")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

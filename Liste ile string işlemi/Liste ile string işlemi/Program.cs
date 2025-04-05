7dc1e544af2c7cff4e1dae3b8153fac1a0f063fb
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-05 14:24:00 +0300

    Liste ile string işlemi

---
+++ Liste ile string işlemi/Liste ile string işlemi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liste_ile_string_işlemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<char> noktalama = new List<char>{ '.', ',', ';', '%', '#', '*', '?', '!','+','-' };
+
+            Console.WriteLine("Hacı içinde noktalama ve özel karakter içeren bir cümle yaz!!");
+
+            string cumle = Console.ReadLine();
+
+            string yeniCumle = "";
+
+            for(int i = 0; i < cumle.Length; i++)
+            {
+                if (!(noktalama.Contains(cumle[i]))) {
+
+                    yeniCumle += cumle[i];
+                
+                }
+
+
+            }
+
+            Console.WriteLine(yeniCumle);
+
+        }
+    }
+}

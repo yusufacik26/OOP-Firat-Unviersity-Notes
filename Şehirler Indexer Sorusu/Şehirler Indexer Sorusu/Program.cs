ece0086dc54cb12feb2cdc112e8e13ad74fefbcc
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-05 14:22:07 +0300

    Sehirler için basit indexer

---
+++ Şehirler Indexer Sorusu/Şehirler Indexer Sorusu/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Şehirler_Indexer_Sorusu
+{
+    class sehir
+    {
+        string[] sehirler = { "", "eskisehir", "ankara", "istanbul", "izmir", "bursa"};
+
+        public int sehirBul(string s)
+        {
+            for(int i = 0; i < sehirler.Length; i++)
+            {
+                if (sehirler[i] == s) { return i; }
+
+
+            }
+            return -1;
+
+        }
+
+        public int this[string s]
+        {
+            get { return sehirBul(s); }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

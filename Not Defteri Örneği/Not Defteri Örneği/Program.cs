980f80815a59291043f5132923d6d499b5557157
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-05 14:24:40 +0300

    Not defteri örneğ,

---
+++ Not Defteri Örneği/Not Defteri Örneği/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Not_Defteri_Örneği
+{
+    class notDefter
+    {
+        private Dictionary<string, int> notlar = new Dictionary<string, int>()
+    {
+        { "Ayşe", 85 },
+        { "Fatma", 90 }
+    };
+
+        public int this[string isim]
+        {
+            get
+            {
+                if (notlar.ContainsKey(isim)){
+                    return notlar[isim];
+                }
+                return -1;
+            }
+
+            set { notlar[isim] = value; }
+
+        }
+        public string this[int index]
+        {
+            get { if (notlar[index] == index) {
+
+                    return notlar[index];
+                
+                } }
+
+        }
+
+
+
+
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            notDefter nots = new notDefter();
+
+            Console.WriteLine(nots+nots["Ayşe"]);
+        }
+    }
+}
---
+++ Not Defteri Örneği/Not Defteri Örneği/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Not Defteri Örneği")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Not Defteri Örneği")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("877fd9ac-b246-4bdd-a0cf-b2ade466238d")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

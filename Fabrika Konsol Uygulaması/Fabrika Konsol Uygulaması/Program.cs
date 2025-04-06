be652e40a9feae8f008f9b6ae370faa85ac22396
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-06 16:09:05 +0300

    Fabrika KONSOL

---
+++ Fabrika Konsol Uygulaması/Fabrika Konsol Uygulaması/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika_Konsol_Uygulaması
+{
+    class fabrika {
+
+        List<isci> elemanlar = new List<isci>();
+
+        public void ElemanEkle(isci i) {
+
+            elemanlar.Add(i);
+        
+        }
+
+        public List<isci> CalisanlariListele()
+        {
+            return elemanlar;
+
+        }
+    
+    }
+    class isci { 
+    
+        public int sirketKimlikNo
+        {
+
+            get;
+            set;
+        }
+
+        public double Maas
+        {
+            get;
+            set;
+
+        }
+
+        public TCkimlik kimlik
+        {
+            get;
+            set;
+        }
+
+
+    }
+
+    class TCkimlik {
+    public int TCNo
+        {
+            get;
+            set;
+
+        }
+    public string ad
+        {
+            get;
+            set;
+
+        }       
+    public string soyad
+        {
+            get;
+            set;
+
+
+        }   
+        public string AdSoyad
+        {
+
+            get { string adsoyad = (this.ad + " " + this.soyad).ToUpper();
+
+                return adsoyad;
+            
+            }
+            
+
+        }
+
+        public string dogumYeri
+        {
+
+            get;
+            set;
+        }
+
+        public int dogumTarihi
+        {
+
+            get;
+            set;
+        }
+
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

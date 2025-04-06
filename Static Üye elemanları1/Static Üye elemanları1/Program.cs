2ba37065c756d7bac236d8ec512c4298cb66056a
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-06 16:07:14 +0300

    Static Üye elemanları1

---
+++ Static Üye elemanları1/Static Üye elemanları1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Üye_elemanları1
+{
+
+    class statikMetod
+    {
+        public static void sMetod1() {
+
+            Console.WriteLine("Statik Metod 1");
+
+            }
+
+        static statikMetod()
+        {
+            Console.WriteLine("Statik çalıştı");
+
+
+        }
+
+        public statikMetod()
+        {
+
+            Console.WriteLine("Statik olmayan çalıştı");
+
+        }
+
+
+}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            statikMetod s = new statikMetod();
+        }
+    }
+}

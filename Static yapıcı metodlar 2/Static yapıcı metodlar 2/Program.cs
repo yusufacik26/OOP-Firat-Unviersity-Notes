98c2464f8b08980c41d982f06e83f986df86f704
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-06 16:07:47 +0300

    Static yapıcı metodlar 2

---
+++ Static yapıcı metodlar 2/Static yapıcı metodlar 2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_yapıcı_metodlar_2
+{
+
+    class Oyuncu
+    {
+        static int toplam;
+
+        public Oyuncu()
+        {
+            toplam++;
+            Console.WriteLine("Toplam Oyuncu " + toplam);
+
+
+        }
+
+        static Oyuncu() {
+            Console.WriteLine("Oyun Başladı!!!!!");
+        }
+
+
+        ~Oyuncu()
+        {
+            Console.WriteLine("Bir Oyuncu Ayrıldı!!!");
+            toplam--;
+
+
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Oyuncu yusuf = new Oyuncu();
+            Oyuncu emir = new Oyuncu();
+            Oyuncu semra = new Oyuncu();
+            Oyuncu musa = new Oyuncu();
+
+        }
+    }
+}

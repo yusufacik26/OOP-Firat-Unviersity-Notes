5f87ee08a0621f65932a2c23f92757a13240cc70
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-06 16:07:29 +0300

    Static Yapıcı Metodlar

---
+++ Static Yapıcı Metodlar/Static Yapıcı Metodlar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Yapıcı_Metodlar
+{
+
+    class Deneme
+    {
+         static Deneme()
+        {
+            Console.WriteLine("Static Metod çalıştı!!!!!");
+
+
+        }
+
+        public Deneme() {
+            Console.WriteLine("Static OLMAYAN Metod çalıştı!!!!!");
+
+
+
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deneme d1 = new Deneme();
+
+            Deneme d2 = new Deneme();
+            Deneme d3 = new Deneme();
+            Deneme d4 = new Deneme();
+        }
+    }
+}

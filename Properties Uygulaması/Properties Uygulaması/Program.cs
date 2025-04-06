14ea8338d90abc38059f41989b529b63051aadb3
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-06 16:06:24 +0300

    Properties Uygulaması

---
+++ Properties Uygulaması/Properties Uygulaması/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_Uygulaması
+{
+    class isci
+    {
+        private int kimlikno;
+
+        public int Kimlikno {
+            get { return kimlikno; }
+            set { kimlikno = value; }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            isci i = new isci();
+            i.Kimlikno = 123;
+
+            Console.WriteLine(i.Kimlikno);
+        }
+    }
+}

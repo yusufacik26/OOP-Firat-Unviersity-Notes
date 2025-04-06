aba9f8472075ac40c1b2e4ebcd40ee1b08c0600e
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-06 16:08:01 +0300

    Static veya Non-Static Olmak

---
+++ Static veya Non-Static Olmak/Static veya Non-Static Olmak/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_veya_Non_Static_Olmak
+{
+    class Program
+    {
+        class Urun
+        {
+            public int id;
+            public string ad;
+            public int fiyat;
+
+            public static int KDVoran;
+
+            public void UrunYaz()
+            {
+
+                Console.WriteLine($"Ürün ID:{id}  Ürün ADI:{ad}  ÜrünFiyatı{fiyat}  KDV Oranı {KDVoran}");
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Urun.KDVoran = 15;
+            Urun klavye = new Urun();
+
+            klavye.ad = "Logitech";
+            klavye.id = 123;
+            klavye.fiyat = 1200;
+
+            klavye.UrunYaz();
+
+
+            Urun mouse = new Urun();
+
+            mouse.ad = "Rampage";
+            mouse.id = 354;
+            mouse.fiyat = 870;
+
+            mouse.UrunYaz();
+
+        }
+    }
+}

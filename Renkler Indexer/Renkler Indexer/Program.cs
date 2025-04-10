58b8768ca02179d3f7cf0cf7ab727862c050a457
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-10 13:13:44 +0300

    Renkler Indexer

---
+++ Renkler Indexer/Renkler Indexer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renkler_Indexer
+{
+    class renkler
+    {
+        string[] renk = { "", "mav", "yeş", "sar", "mor", "tur", "bor" };
+
+        public int renkBul(string renkGirilen)
+        {
+
+            for(int i =0; i < renk.Length; i++)
+            {
+                if (renkGirilen == renk[i])
+                {
+                    return i;
+
+                }
+
+
+            }
+            return -1;
+        }
+
+        public int this[string girilenAy]
+        {
+            get { return renkBul(girilenAy); }
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            renkler r = new renkler();
+
+            Console.WriteLine(r.renkBul("mav"));
+
+        }
+    }
+}
---
+++ Renkler Indexer/Renkler Indexer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Renkler Indexer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Renkler Indexer")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("ec38eb18-6c17-41d6-9e2f-9f8643799f38")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

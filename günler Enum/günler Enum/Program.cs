dcb06d3dd84e477325a7371e5a03c66244c96e57
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-12 16:41:10 +0300

    Enum gün örneği

---
+++ günler Enum/günler Enum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static günler_Enum.Program;
+
+namespace günler_Enum
+{
+    class Program
+    {
+        public enum Gun {pazartesi,sali,carsamba,persembe,cuma,cumartesi,pazar }
+        public static Gun GunOtele(Gun g, int kacGunSonra)
+        {
+            return (Gun)(((int)g + kacGunSonra) % 7);
+        }
+        static void Main(string[] args)
+        {
+            Gun bugun = Gun.carsamba;
+            Gun sonra = GunOtele(bugun, 3);
+            Console.WriteLine("3 gün sonra: " + sonra); // Cumartesi
+        }
+    }
+}

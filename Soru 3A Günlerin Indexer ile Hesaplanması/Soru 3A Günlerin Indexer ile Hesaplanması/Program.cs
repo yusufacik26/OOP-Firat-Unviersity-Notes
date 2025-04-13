63f0515518f50d72fa8f082fd9e33681fd357fba
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-13 23:49:25 +0300

    indeksleme çalışması

---
+++ Soru 3A Günlerin Indexer ile Hesaplanması/Soru 3A Günlerin Indexer ile Hesaplanması/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soru_3A_Günlerin_Indexer_ile_Hesaplanması
+{
+
+    class gunler
+    {
+        string[] gun = { "", "pzt", "sal", "car", "per", "cum", "cmt", "paz" };
+
+        public int gunBul(string aranan)
+        {
+            for(int i=0;i<gun.Length; i++)
+            {
+                if (aranan.Equals(gun[i])){
+
+                    return i;
+                }
+            }
+            return -1;
+
+        }
+
+        public int this[string isim]
+        {
+
+            get { return gunBul(isim); }
+
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            gunler g = new gunler();
+            Console.WriteLine("Gün giriniz:");
+            string giris = Console.ReadLine();
+            int sonuc = g[giris];
+            Console.WriteLine(sonuc > 0 ? "Sırası: " + sonuc : "Geçersiz gün");
+        }
+    }
+}

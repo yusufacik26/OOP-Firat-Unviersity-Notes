4ced687f37f6e516ccd18f6a9b20a3675107a9a9
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-16 23:02:03 +0300

    Mevsim eşleme

---
+++ Mevsim eşleme/Mevsim eşleme/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mevsim_eşleme
+{
+    class mevsim
+    {
+
+        public string[] mevsimler = { "", "oca", "sub", "mar", "nis", "may", "haz", "tem", "agu", "eyl", "eki", "kas", "ara" };
+
+        public int mevsimBul(string girdi)
+        {
+            for(int i = 0; i < mevsimler.Length; i++)
+            {
+                if (mevsimler[i] == girdi) { return i; }
+
+
+            }
+            return -1;
+        }
+
+        public string this[string girdi]
+        {
+            get
+            {
+                int aySayi = mevsimBul(girdi);
+
+                if (aySayi <= 2 || aySayi==12) { return "Kış"; }
+                else if(aySayi>=3 && aySayi <= 5) { return "İlkBahar"; }
+                else if (aySayi >= 6 && aySayi <= 8) { return "Yaz"; }
+                else{ return "SonBahar"; }
+
+            }
+
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mevsim m = new mevsim();
+
+             string mevsimi = m["oca"];
+            Console.WriteLine("Mevsim: " +mevsimi );
+        }
+    }
+}

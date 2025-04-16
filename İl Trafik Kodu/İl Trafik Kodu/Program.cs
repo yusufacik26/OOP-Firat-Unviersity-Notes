9f566d0c7dad5a7770b69ef3208a6d77ee6575d1
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2025-04-16 23:01:29 +0300

    İl Trafik Kodu

---
+++ İl Trafik Kodu/İl Trafik Kodu/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İl_Trafik_Kodu
+{
+    class plaka
+    {
+        public string[] plakalar = { "", "Adana", "Adıyaman", "Afyon","Eskisehir", "İstanbul" };
+
+
+        public int sehirBul(string sehir)
+        {
+            for(int i = 0; i < plakalar.Length; i++)
+            {
+                if (plakalar[i] == sehir) { return i; }
+
+
+            }
+            return -1;
+
+        }
+
+        public int this[string girdi]
+        {
+            get { return sehirBul(girdi); }
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            plaka p = new plaka();
+
+            Console.WriteLine(p.sehirBul("Eskisehir"));
+        }
+    }
+}
